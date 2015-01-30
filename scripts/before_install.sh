f7139176f207acaa6f1a5caffc052fe543d73b3e
Nancho <nancho@kibostudios.com>
2015-01-30 12:36:03 +0100

    Add sudo when adding a repository to the list

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,12 +5,12 @@ sudo apt-get update
 sudo apt-get install curl -y
 
 sudo curl http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo echo deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main > /etc/apt/sources.list.d/elasticsearch.list
+sudo echo deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main > sudo /etc/apt/sources.list.d/elasticsearch.list
 
 sudo curl http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-sudo echo deb http://nginx.org/packages/ubuntu/ trusty nginx > /etc/apt/sources.list.d/nginx.list
+sudo echo deb http://nginx.org/packages/ubuntu/ trusty nginx > sudo /etc/apt/sources.list.d/nginx.list
 
-sudo echo deb http://packages.elasticsearch.org/logstash/1.4/debian stable main > /etc/apt/sources.list.d/logstash.list
+sudo echo deb http://packages.elasticsearch.org/logstash/1.4/debian stable main > sudo /etc/apt/sources.list.d/logstash.list
 
 # Install ELK and nginx
 sudo apt-get update
