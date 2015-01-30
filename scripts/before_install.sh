15dce2cffed467c96732339fffc9fea9361c6396
Nancho <nancho@kibostudios.com>
2015-01-30 12:31:01 +0100

    Back to sudo command on each line

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,67 +1,65 @@
 #!/bin/bash
 
-sudo su
-
 # Install curl and download keys
-apt-get update
-apt-get install curl -y
+sudo apt-get update
+sudo apt-get install curl -y
 
-curl http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
-echo deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main > /etc/apt/sources.list.d/elasticsearch.list
+sudo curl http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo echo deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main > /etc/apt/sources.list.d/elasticsearch.list
 
-curl http://nginx.org/keys/nginx_signing.key | apt-key add -
-echo deb http://nginx.org/packages/ubuntu/ trusty nginx > /etc/apt/sources.list.d/nginx.list
+sudo curl http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+sudo echo deb http://nginx.org/packages/ubuntu/ trusty nginx > /etc/apt/sources.list.d/nginx.list
 
-echo deb http://packages.elasticsearch.org/logstash/1.4/debian stable main > /etc/apt/sources.list.d/logstash.list
+sudo echo deb http://packages.elasticsearch.org/logstash/1.4/debian stable main > /etc/apt/sources.list.d/logstash.list
 
 # Install ELK and nginx
-apt-get update
-apt-get install openjdk-7-jdk elasticsearch logstash=1.4.2-1-2c0f5a1 nginx -y
+sudo apt-get update
+sudo apt-get install openjdk-7-jdk elasticsearch logstash=1.4.2-1-2c0f5a1 nginx -y
 
 # Install Kibana
-mkdir /opt/kibana \
-curl -O https://download.elasticsearch.org/kibana/kibana/kibana-3.1.1.tar.gz \
-tar xvf kibana-3.1.1.tar.gz -C /opt/kibana --strip-components=1 \
-rm -f kibana-3.1.1.tar.gz
+sudo mkdir /opt/kibana \
+sudo curl -O https://download.elasticsearch.org/kibana/kibana/kibana-3.1.1.tar.gz \
+sudo tar xvf kibana-3.1.1.tar.gz -C /opt/kibana --strip-components=1 \
+sudo rm -f kibana-3.1.1.tar.gz
 
 # Configure and start elasticsearch
-cp ~/elk/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp ~/elk/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 
 
 ### configure and start logstash
 
 # cert/key
-mkdir -p /etc/pki/tls/certs
-mkdir /etc/pki/tls/private
-cp ~/elk/logstash-forwarder.crt /etc/pki/tls/certs/logstash-forwarder.crt
-cp ~/elk/logstash-forwarder.key /etc/pki/tls/private/logstash-forwarder.key
+sudo mkdir -p /etc/pki/tls/certs
+sudo mkdir /etc/pki/tls/private
+sudo cp ~/elk/logstash-forwarder.crt /etc/pki/tls/certs/logstash-forwarder.crt
+sudo cp ~/elk/logstash-forwarder.key /etc/pki/tls/private/logstash-forwarder.key
 
 # filters
-cp ~/elk/01-logstash-input.conf /etc/logstash/conf.d/01-logstash-input.conf
-cp ~/elk/10-syslog.conf /etc/logstash/conf.d/10-syslog.conf
-cp ~/elk/11-nginx.conf /etc/logstash/conf.d/11-nginx.conf
+sudo cp ~/elk/01-logstash-input.conf /etc/logstash/conf.d/01-logstash-input.conf
+sudo cp ~/elk/10-syslog.conf /etc/logstash/conf.d/10-syslog.conf
+sudo cp ~/elk/11-nginx.conf /etc/logstash/conf.d/11-nginx.conf
 
 # patterns
-cp ~/elk/nginx.pattern /opt/logstash/patterns/nginx
-chown logstash:logstash /opt/logstash/patterns/nginx
+sudo cp ~/elk/nginx.pattern /opt/logstash/patterns/nginx
+sudo chown logstash:logstash /opt/logstash/patterns/nginx
 
 ### configure kibana
 
-cp ~/elk/kibana-config.js /opt/kibana/config.js
+sudo cp ~/elk/kibana-config.js /opt/kibana/config.js
 
 
 ### configure and start nginx
 
-cp ~/elk/nginx.conf /etc/nginx/nginx.conf
-echo "daemon off;" >> /etc/nginx/nginx.conf
+sudo cp ~/elk/nginx.conf /etc/nginx/nginx.conf
+sudo echo "daemon off;" >> /etc/nginx/nginx.conf
 
-cp ~/elk/nginx-site.conf /etc/nginx/conf.d/default.conf
+sudo cp ~/elk/nginx-site.conf /etc/nginx/conf.d/default.conf
 
-cp ~/elk/start.sh /usr/local/bin/start.sh
-chmod +x /usr/local/bin/start.sh
+sudo cp ~/elk/start.sh /usr/local/bin/start.sh
+sudo chmod +x /usr/local/bin/start.sh
 
 # Start all the services
 
-service elasticsearch start
-service logstash start
-service nginx start
+sudo service elasticsearch start
+sudo service logstash start
+sudo service nginx start
