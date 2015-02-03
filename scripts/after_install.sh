48a4bb2b8b7c0ff86856d54957bb8f857d110e00
Nancho <nancho@kibostudios.com>
2015-02-03 17:10:57 +0100

    Better files organization

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Configure logstash
-sudo cp ~/elk/01-logstash-input.conf /etc/logstash/conf.d/01-logstash-input.conf
-sudo cp ~/elk/11-nginx.conf /etc/logstash/conf.d/11-nginx.conf
-sudo cp ~/elk/99-output.conf /etc/logstash/conf.d/99-output.conf
+sudo cp ~/elk/config/01-logstash-input.conf /etc/logstash/conf.d/01-logstash-input.conf
+sudo cp ~/elk/config/11-nginx.conf /etc/logstash/conf.d/11-nginx.conf
+sudo cp ~/elk/config/99-output.conf /etc/logstash/conf.d/99-output.conf
 
-sudo cp ~/elk/logstash-web.conf /etc/init/logstash-web.conf
+sudo cp ~/elk/config/logstash-web.conf /etc/init/logstash-web.conf
 
 # patterns
-sudo cp ~/elk/nginx.pattern /opt/logstash/patterns/nginx
+sudo cp ~/elk/config/nginx.pattern /opt/logstash/patterns/nginx
 sudo chown logstash:logstash /opt/logstash/patterns/nginx
 
 # Configure nginx
-sudo cp ~/elk/nginx-site.conf /etc/nginx/sites-available/default
+sudo cp ~/elk/config/nginx-site.conf /etc/nginx/sites-available/default
 
 # Start all the services
 sudo service elasticsearch start
